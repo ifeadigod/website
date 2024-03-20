9639ec43c9d0ebb9b6a0cafe9361c225fe84da58
Hp <Hp@DESKTOP-VQO4T31>
2024-03-21 00:12:06 +0100

    My Website

---
+++ src/header.js
@@ -0,0 +1,23 @@
+function Header(){
+    return(
+        <div class="header">
+            <div class="logo">
+                <h1>Tutor</h1>
+            </div>
+            <div class="nav-bar">
+                <a href="" id="home">Home</a>
+                <a href="">About</a>
+                <a href="">Resources</a>
+                <a href="">Contact us</a>
+            </div>
+            <div class="buttons">
+                <button id="sign-in">
+                    sign in
+                </button>
+                <button id="register">Register</button>
+            </div>
+      </div>
+    );
+}
+
+export default Header;
